d01bff7460b7975c3ef86846b6c28bb50de6554d
kdm <ehdals0116@gmail.com>
2020-12-14 20:39:49 +0900

    2020-12-14 exampleSpringBoot ex10

---
+++ ex10_validationutils/src/main/java/com/study/springBoot/Ex10ValidationutilsApplication.java
@@ -0,0 +1,13 @@
+package com.study.springBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex10ValidationutilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ex10ValidationutilsApplication.class, args);
+    }
+
+}
